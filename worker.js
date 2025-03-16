05d49ee05ec6d032ce2e48560f83f1b7e2464691
gingrspacecadet <jackrawlings2011@gmail.com>
2025-03-16 13:09:02 +0000

    Delete worker.js

--- worker.js
+++
@@ -1,6 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const { results } = await env.DB.prepare("SELECT * FROM users").all();
-    return new Response(JSON.stringify(results), { headers: { "Content-Type": "application/json" } });
-  },
-};
