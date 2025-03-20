dd9cc1556d2a72b11801b845ce1881a179322b4e
Oddlaceguy49 <116045403+Oddlaceguy49@users.noreply.github.com>
2025-03-20 16:56:29 -0700

    Update image_conversion.mjs

--- scripts/image_conversion.mjs
+++ scripts/image_conversion.mjs
@@ -31,7 +31,7 @@ export async function convertToWebPBase64(file) {
           const webpBuffer = await webpBlob.arrayBuffer();
           const webpBase64 = btoa(String.fromCharCode.apply(null, new Uint8Array(webpBuffer)));
 
-          return = webpBase64;
+          return "data:image/webp;base64" + webpBase64;
         };
         img.src = event.target.result;
       };
